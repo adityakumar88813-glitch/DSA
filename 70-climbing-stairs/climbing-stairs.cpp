52e6d04b9b68cf4e8e0ac6da79a949dbbbbeadee
Aditya Kumar <adityakumar88813@gmail.com>
2026-06-30 20:49:43 +0530

    Time: 2 ms (4.91%) | Memory: 8.7 MB (18.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+int climb(int n , vector<int>&dp ){
+    // TOP DOWN 
+    //  if(n==0||n==1) return 1;
+
+    //  if(dp[n] != -1) return dp[n];
+    //  dp[n] =climb(n-1 ,dp) + climb(n-2 ,dp);
+    //  return dp[n];
+
+
+    //DOWN UP
+    dp[0] = 1;
+    dp[1] = 1;
+    for(int i = 2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
+}
+    int climbStairs(int n) {
+    vector<int>dp(n+1);
+    return climb(n , dp);
+    }
+};
