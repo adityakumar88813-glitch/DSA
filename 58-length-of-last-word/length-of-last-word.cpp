cc1a0fc20f98fd54072c1919bf06e4b46ff84c9e
Aditya Kumar <adityakumar88813@gmail.com>
2026-08-05 23:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (34.79%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            if (s[i] == ' ' && count == 0) {
+                continue;           // Skip trailing spaces
+            }
+
+            if (s[i] != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+};
