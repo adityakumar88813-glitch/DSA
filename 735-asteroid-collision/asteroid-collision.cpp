869151f975738050d5aed5526edc2befa9e7d916
Aditya Kumar <adityakumar88813@gmail.com>
2026-08-14 23:47:44 +0530

    Time: 4 ms (14.19%) | Memory: 22.3 MB (19.01%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        int n = asteroids.size();
+
+        stack<int> st;
+
+        for(int i = 0; i < n; i++) {
+
+            if(asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            }
+            else {
+
+                while(!st.empty() && st.top() > 0 &&
+                      st.top() < abs(asteroids[i])) {
+                    st.pop();
+                }
+
+                if(st.empty() || st.top() < 0) {
+                    st.push(asteroids[i]);
+                }
+                else if(st.top() == abs(asteroids[i])) {
+                    st.pop();
+                }
+            }
+        }
+
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();   // important
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
