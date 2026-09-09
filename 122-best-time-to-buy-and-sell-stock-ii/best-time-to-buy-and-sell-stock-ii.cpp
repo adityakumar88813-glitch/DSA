9e286087b076f89e510a2898ef81dfc7850730e5
Aditya Kumar <adityakumar88813@gmail.com>
2026-09-09 18:50:42 +0530

    Time: 15 ms (13.91%) | Memory: 27.4 MB (18.01%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,61 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int total_profit = 0;
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         int total_profit = 0;
 
-        for(int i = 0; i < n - 1; i++) {
-            if(prices[i] < prices[i + 1]) {
-                total_profit += prices[i + 1] - prices[i];
-            }
+//         for(int i = 0; i < n - 1; i++) {
+//             if(prices[i] < prices[i + 1]) {
+//                 total_profit += prices[i + 1] - prices[i];
+//             }
+//         }
+
+//         return total_profit;
+//     }
+// };
+
+
+
+
+// USING DP
+ 
+ class Solution {
+public:
+
+    int solve(int i, int buy, vector<int>& prices ,  vector<vector<int>> &dp) {
+        // Base case
+        if (i == prices.size())
+            return 0;
+        int profit = 0;
+
+        if(dp[i][buy] != -1) return dp[i][buy];
+
+        if (buy) {
+            // Buy
+            int buyStock = -prices[i] + solve(i + 1, 0, prices,dp);
+
+            // Skip buying
+            int skip = solve(i + 1, 1, prices ,dp);
+
+            profit = max(buyStock, skip);
+        }
+        else {
+            // Sell
+            int sellStock = prices[i] + solve(i + 1, 1, prices,dp);
+
+            // Skip selling
+            int skip = solve(i + 1, 0, prices,dp);
+
+            profit = max(sellStock, skip);
         }
 
-        return total_profit;
+        return dp[i][buy]  =  profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+         int n = prices.size();
+         vector<vector<int>> dp(n , vector<int>(2,-1));
+        return solve(0, 1, prices ,dp);
     }
 };
+
