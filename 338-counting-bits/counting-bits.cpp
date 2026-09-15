6ce2f968827472132688264ac2327b881a70b760
Aditya Kumar <adityakumar88813@gmail.com>
2026-09-16 00:07:00 +0530

    Time: 2 ms (35.07%) | Memory: 10.7 MB (57.10%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+        vector<int> ans(n + 1);
+
+        ans[0] = 0;
+
+        if (n >= 1)
+            ans[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            if (i % 2 != 0) {
+                ans[i] = ans[i / 2] + 1;
+            }
+            else {
+                ans[i] = ans[i / 2];
+            }
+        }
+
+        return ans;
+    }
+};
